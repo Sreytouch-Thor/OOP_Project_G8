9c87c0780f23f835e690b14764bb7515274ff752
linnamuth <muthlinna73gmail.com>
2023-04-27 13:04:17 +0700

    update crate some class

--- src/Airport.ts
+++ src/Airport.ts
@@ -1,2 +1,12 @@
 
+export class Airport {
+    private departurArea:string
+    private CheckIn:string
+    constructor(departurArea:string,CheckIn:string)
+    {
+        this.departurArea= departurArea
+        this.CheckIn= CheckIn
+       
+    }
+
 }
